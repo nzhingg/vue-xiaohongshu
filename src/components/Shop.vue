85e2004ac3f5812c3a237994cd4141c754a75799
nzhingg <2258190228@qq.com>
2018-03-14 19:20:25 +0800

    Shop

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="shop">
     <NoteHead />
+    <div class="empty">暂时未开发</div>
   </div>
 </template>
 
@@ -16,4 +17,15 @@ export default {
 </script>
 
 <style scoped>
+.empty{
+  width: 10rem;
+  position: relative;
+  top: 2.613333rem /* 196/75 */;
+  background-color: #FFF;
+  font-size: .533333rem /* 40/75 */;
+  color: #FF2640;
+  font-weight: 1000;
+  margin-top: 4rem /* 300/75 */;
+  text-align: center;
+}
 </style>
